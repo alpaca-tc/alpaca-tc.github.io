f951be33c786879c0ca9c5a3587cb9afd6241de5
alpaca-tc <alpaca-tc@alpaca.tc>
2021-04-03 23:55:15 +0900

    Initialize nextjs

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  blogName: 'RailsとRubyとVimのブログ',
+  defaultTitle: 'RailsとRubyとVimのブログ',
+  description: '',
+  defaultLanguage: 'ja',
+  locale: 'ja_JP'
+}
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+//
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   // ...
+// }
+//
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // ...
+// }
+//
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // ...
+// }
+
+export default () => {
+  return <div>Hello world</div>
+}
